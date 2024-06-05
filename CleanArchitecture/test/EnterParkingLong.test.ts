42c7f7a351ee8c169ddfb8f21eb54e675b6cc2e8
Guilherme China <echina725@gmail.com>
2024-06-05 11:55:19 -0300

    implements repository

--- CleanArchitecture/test/EnterParkingLong.test.ts
+++ CleanArchitecture/test/EnterParkingLong.test.ts
@@ -1,8 +1,10 @@
 import EnterParkingLot from "../source/core/usecase/EnterParkingLot";
+import ParkingLotRepositoryMemory from "../source/infra/repository/ParkingLotRepositoryMemory";
 
 // the idea is a test to use a Use Case
-test("Should enter parking lot", function() {
-    const enterParkingLot = new EnterParkingLot();
-    const parkingLot = enterParkingLot.execute();
+test("Should enter parking lot", async function() {
+    const parkingLotRepositoryMemory = new ParkingLotRepositoryMemory();
+    const enterParkingLot = new EnterParkingLot(parkingLotRepositoryMemory);
+    const parkingLot = await enterParkingLot.execute("shopping");
     expect(parkingLot.code).toBe("shopping");
 })
