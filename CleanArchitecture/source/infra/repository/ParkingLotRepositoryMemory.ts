b613c5b40bac2573b1f42135f82e2b132757afb2
Guilherme China <echina725@gmail.com>
2024-06-05 12:55:52 -0300

    things just...

--- CleanArchitecture/source/infra/repository/ParkingLotRepositoryMemory.ts
+++ CleanArchitecture/source/infra/repository/ParkingLotRepositoryMemory.ts
@@ -1,8 +1,19 @@
+import ParkedCar from "../../core/entity/ParkedCar";
 import ParkingLot from "../../core/entity/ParkingLot";
 import ParkingLotRepository from "../../core/repository/ParkingLotRepository";
 
 export default class ParkingLotRepositoryMemory implements ParkingLotRepository {
+
+    parkingLots: ParkingLot[] = [
+        new ParkingLot("shopping", 100, 8, 22)
+    ]
+    public parkedCars: ParkedCar[] = []
+
     getParkingLot(code: string): Promise<ParkingLot> {
-        return Promise.resolve(new ParkingLot(code, 100, 8, 22));
+        return Promise.resolve(this.parkingLots.find(parkingLot => parkingLot.code === code));
+    }
+
+    saveParkedCar(code: string, plate: string, date: Date): void {
+
     }
 }
